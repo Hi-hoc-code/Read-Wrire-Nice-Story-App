74939316823554595a23ef169e49c91bb836e6a4
Ginh1 <quangvinh3122@gmail.com>
2023-11-09 22:25:05 +0700

    Update 2 register feature
    Add user into realtime database.

--- app/src/main/java/com/example/read_write_app_duan1/activities/RegisterActivity.java
+++ app/src/main/java/com/example/read_write_app_duan1/activities/RegisterActivity.java
@@ -15,14 +15,18 @@ import android.widget.Toast;
 
 import com.example.read_write_app_duan1.MainActivity;
 import com.example.read_write_app_duan1.R;
+import com.example.read_write_app_duan1.models.Users;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
     EditText edtRegisterEmail, edtRegisterUsername, edtRegisterPass, edtDateOfBirth;
     Button btnRegister;
     @Override
@@ -31,6 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance(); // Initialize FirebaseAuth
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //Ánh xạ
         edtRegisterEmail = findViewById(R.id.edtRegisterEmail);
@@ -44,11 +49,27 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = edtRegisterEmail.getText().toString();
                 String password = edtRegisterPass.getText().toString();
-                createAccount(email, password);
+                String username = edtRegisterUsername.getText().toString();
+                String dateOfBirth = edtDateOfBirth.getText().toString();
+
+                if (email.isEmpty()) {
+                    edtRegisterEmail.setError("Email cannot be empty");
+                }
+                if (password.isEmpty()) {
+                    edtRegisterPass.setError("Password cannot be empty");
+                }
+                if (username.isEmpty()) {
+                    edtRegisterUsername.setError("Username cannot be empty");
+                }
+                if (dateOfBirth.isEmpty()) {
+                    edtDateOfBirth.setError("Date of birth cannot be empty");
+                } else {
+                    createAccount(email, password, username, dateOfBirth);
+                }
             }
         });
     }
-    private void createAccount(String email, String pass) {
+    private void createAccount(String email, String pass, String username, String dateOfBirth) {
         mAuth.createUserWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -56,6 +77,7 @@ public class RegisterActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "Đăng ký thành công");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            writeNewUser(user.getUid(),email, username, pass, dateOfBirth);
                             Toast.makeText(RegisterActivity.this, "Đăng ký thành công",
                                     Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(RegisterActivity.this, MainActivity.class));
@@ -66,5 +88,10 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+    public void writeNewUser(String userId, String email, String username, String password, String dateOfBirth) {
+        Users user = new Users(userId, email, username, password, dateOfBirth);
+
+        mDatabase.child("user").child(userId).setValue(user);
     }
 }
