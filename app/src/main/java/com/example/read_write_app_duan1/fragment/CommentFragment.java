4ebdda85e0effab35a4556e1396e27c2c7ef8cd0
Ginh1 <quangvinh3122@gmail.com>
2023-11-22 13:33:12 +0700

    Cap nhat comment

--- app/src/main/java/com/example/read_write_app_duan1/fragment/CommentFragment.java
+++ app/src/main/java/com/example/read_write_app_duan1/fragment/CommentFragment.java
@@ -8,42 +8,57 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.read_write_app_duan1.R;
 import com.example.read_write_app_duan1.adapter.CommentAdapter;
 import com.example.read_write_app_duan1.models.Comment;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CommentFragment extends Fragment {
     RecyclerView rcvComment;
     CommentAdapter commentAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_comment, container, false);
 
-        rcvComment = view.findViewById(R.id.rcvComment);
-        FirebaseRecyclerOptions<Comment> options =
-                new FirebaseRecyclerOptions.Builder<Comment>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("items"), Comment.class)
-                        .build();
+        String bookId = getArguments().getString("bookId");
+        if (bookId != null) {
+            rcvComment = view.findViewById(R.id.rcvComment);
+            rcvComment.setLayoutManager(new LinearLayoutManager(getContext()));
+            rcvComment.setAdapter(commentAdapter);
+            // Use bookId to construct Firebase database reference
+            DatabaseReference commentsRef = FirebaseDatabase.getInstance().getReference()
+                    .child("Book")
+                    .child(bookId)  // Ensure bookId is initialized
+                    .child("description");
 
-        commentAdapter = new CommentAdapter(options);
-
-        rcvComment.setAdapter(commentAdapter);
+            // Create FirebaseRecyclerOptions and pass it to the adapter
+            FirebaseRecyclerOptions<Comment> options = new FirebaseRecyclerOptions.Builder<Comment>()
+                    .setQuery(commentsRef, Comment.class)
+                    .build();
+            // Use the book ID to load comments from Firebase
+            commentAdapter = new CommentAdapter(options);
+            rcvComment.setAdapter(commentAdapter);
+        }
         return view;
     }
     @Override
     public void onStart() {
         super.onStart();
-        commentAdapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        commentAdapter.stopListening();
     }
 }
