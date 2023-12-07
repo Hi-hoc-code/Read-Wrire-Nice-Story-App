3637e2e8d793654e7087b3b8e21c64b0c62e36e9
lequanghoa340 <125871175+lequanghoa340@users.noreply.github.com>
2023-12-07 22:52:16 +0700

    Create AdminActivity

--- app/src/main/java/com/example/read_write_app_duan1/activities/AdminActivity.java
+++ app/src/main/java/com/example/read_write_app_duan1/activities/AdminActivity.java
@@ -10,11 +10,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.read_write_app_duan1.R;
 import com.example.read_write_app_duan1.adapter.ListTypeAdapter;
+import com.example.read_write_app_duan1.fragment.HomeFragment;
 import com.example.read_write_app_duan1.fragment.LibraryFragment;
 import com.example.read_write_app_duan1.models.Book;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,24 +35,19 @@ public class AdminActivity extends AppCompatActivity {
     ArrayList<Book> list;
     ListTypeAdapter listTypeAdapter;
     RecyclerView recyclerView;
-    Button back;
+    TextView back, backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
-        Intent intent = getIntent();
-        if (intent != null) {
-            String name = intent.getStringExtra("name");
-
-
-        }
         intUi();
     }
 
     private void intUi() {
         recyclerView = findViewById(R.id.recyView);
         back = findViewById(R.id.back);
+        backHome = findViewById(R.id.BackHome);
 
         //set layout
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -66,18 +65,52 @@ public class AdminActivity extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(AdminActivity.this, LibraryFragment.class));
+                updateCheckBookToZero();
             }
         });
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminActivity.this, HomeFragment.class));
+            }
+        });
+
 
         getdataFromFireBase();
 
     }
 
+    private void updateCheckBookToZero() {
+        // Xác định vị trí cần cập nhật checkBook, ví dụ: ở vị trí đầu tiên trong danh sách list
+        int positionToUpdate = 0; // Đây là ví dụ, bạn cần xác định vị trí cụ thể
 
+        // Kiểm tra xem vị trí có hợp lệ không và nếu có, cập nhật checkBook thành 0 và đẩy lên Firebase
+        if (positionToUpdate >= 0 && positionToUpdate < list.size()) {
+            String bookIdToUpdate = list.get(positionToUpdate).getId();
+            DatabaseReference bookRefToUpdate = FirebaseDatabase.getInstance().getReference().child("Book").child(bookIdToUpdate);
+
+            // Cập nhật giá trị checkBook thành 0
+            bookRefToUpdate.child("checkBook").setValue(0)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Cập nhật thành công
+                            // Refresh RecyclerView nếu cần thiết
+                            listTypeAdapter.notifyDataSetChanged();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Xử lý khi cập nhật thất bại
+                        }
+                    });
+        }
+    }
     //used to update RecyclerView
     private void getdataFromFireBase(){
-        databaseReference.orderByChild("checkBook").equalTo(0).addChildEventListener(new ChildEventListener() {
+        databaseReference.orderByChild("checkBook").equalTo("2").addChildEventListener(new ChildEventListener() {
             //con
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
